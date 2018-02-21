5d61e3217bc8cc62d61c5ab2921e2bde7dae200e
Rodrigo <rodrigo.bacellar@stone.com.br>
2018-02-21 18:58:38 -0300

    DatePicker test

--- app/src/androidTest/java/br/com/androidplayground/register/robots/RegisterDatePickerResult.kt
+++ app/src/androidTest/java/br/com/androidplayground/register/robots/RegisterDatePickerResult.kt
@@ -6,6 +6,7 @@ import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.contrib.PickerActions
 import android.support.test.espresso.matcher.ViewMatchers.*
 import br.com.androidplayground.R
+import java.text.SimpleDateFormat
 import java.util.*
 
 /**
@@ -28,14 +29,15 @@ class RegisterDatePickerResult {
         val month = now.get(Calendar.MONTH)+1
         val day = now.get(Calendar.DAY_OF_MONTH)
 
+        val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+        val formattedDate = sdf.format(now.time)
+
         onView(withId(R.integer.date_picker_id))
                 .perform(PickerActions.setDate(year, month, day))
 
         onView(withText("OK")).perform(click())
 
-        Thread.sleep(4000)
-
-        onView(withId(R.id.date)).check(matches(withText("19/02/2018")))
+        onView(withId(R.id.date)).check(matches(withText(formattedDate)))
 
         return true
     }
